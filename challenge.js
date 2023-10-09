b039df18030ba4e669a1fab46a765449ef645891
= <=>
2023-10-09 20:16:51 +0100

    functions

---
+++ challenge.js
@@ -0,0 +1,86 @@
+
+// // write a for loop that prints your name 20 times
+
+// for(let i = 0; i < 20; i++) {
+//     console.log("Ahmed")
+// }
+
+
+// // write a while that prints your surname 20 times
+// let i = 0;
+// while (i < 20) {
+//     console.log("Dini");
+//     i++;
+// }
+
+
+// write if/else if, chained code to check what day of the day it is and print something
+// let i = 0; 
+// do { 
+//     console.log("hibaq from do while");
+//     i++
+// } while(i < 5)
+    
+
+
+
+// // convert the above if/else to switch
+
+// switch (today) {
+//     case "Monday":
+//         console.log("It is Coding Day, from switch")
+//         break;
+//     case "Tuesday":
+//         console.log("It is my off day, from switch")
+//         break;
+
+//     default:
+//         console.log("ask me personally which day, from switch")
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+ 
+
+
+function multiply (num1,num2) { 
+    let result = num1 * num2;
+    return result;
+}
+
+ let sum = multiply (5,6)
+console.log(sum)
+
+
+
+
+
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result; 
+}
+multiply(num1,num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
